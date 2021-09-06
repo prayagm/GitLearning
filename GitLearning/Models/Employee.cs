405a34763120d918ce128de25e0733f501304167
vishnusb1996 <52482641+vishnusb1996@users.noreply.github.com>
2021-09-06 12:30:37 +0530

    Added new property for Address

--- GitLearning/Models/Employee.cs
+++ GitLearning/Models/Employee.cs
@@ -11,5 +11,7 @@ namespace GitLearning.Models
         public int EmployeeId { get; set; }
 
         public string EmployeeName { get; set; }
+
+        public string Address { get; set; }
     }
 }
